f4afe4aa3333d74126f708380d7b29de40186c6d
Angelica Ramirez <angelicaradz3@gmail.com>
2024-05-17 16:04:00 +0800

    added customized registration page

--- peoplepsyche-app/resources/views/auth/login.blade.php
+++ peoplepsyche-app/resources/views/auth/login.blade.php
@@ -1,47 +1,76 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+@extends('layout.home')
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+@section('body')
+<div class="container d-flex justify-content-center align-items-center min-vh-100">
+    <!-- LOGIN BOX -->
+    <div id="login-page" class="row border rounded-5 p-5 shadow box-area bg-white justify-content-center align-items-center d-flex flex-lg-row">
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        <!-- FORM TITLE -->
+        <div class="row mb-4">
+            <h2 class="text-center">PeoplePsyche Login</h2>
         </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+        <!-- LOGIN FORM SECTION -->
+        <div class="row justify-content-center align-items-center">
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+            <!-- Session Status -->
+            <x-auth-session-status class="mb-4" :status="session('status')" />
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            {{-- LOGIN FORM --}}
+            <form method="POST" id="login-form" class="g-3" action="{{ route('login') }}">
+                @csrf
+
+                <!-- EMAIL ADDRESS INPUT FIELD -->
+                <div class="form-floating mb-2 overflow-hidden">
+                    <x-text-input type="email" class="form-control" id="floatingInput" name="email" placeholder="name@example.com" :value="old('email')" required autofocus autocomplete="username" />
+                    <x-input-label for="floatingInput" :value="__('Email')" />
+                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                </div>
+
+                <!-- PASSWORD INPUT FIELD -->
+                <div class="form-floating overflow-hidden">
+                    <x-text-input class="form-control" id="floatingPassword"
+                                    type="password"
+                                    name="password"
+                                    placeholder="Password"
+                                    required autocomplete="current-password" />
+                    <x-input-label for="floatingPassword" :value="__('Password')" />
+                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                </div>
+
+                <div class="d-flex flex-sm-row mt-1 mb-4 g-4 align-items-center">
+
+                    <!-- SAVE LOGIN INFO OPTION -->
+                    <div class="form-check col d-flex justify-content-start">
+                        <label class="form-check-label text-secondary" for="flexCheckChecked">
+                            <input class="form-check-input" type="checkbox" value="Remember Me" id="flexCheckChecked" name="remember" checked>
+                            <span>{{ __('Remember me') }}</span>
+                        </label>
+                    </div>
+
+                    {{-- FORGOT YOUR PASSWORD OPTION --}}
+                    <div class="col d-flex justify-content-end">
+                        @if (Route::has('password.request'))
+                            <a href="{{ route('password.request') }}" class="text-secondary underline">
+                                {{ __('Forgot your password?') }}
+                            </a>
+                        @endif
+                    </div>
+                </div>
+
+                <!-- SUBMIT BUTTON -->
+                <div class="input-group">
+                    <button class="btn btn-lg w-100 fs-6" type="submit" value="Login">
+                        {{ __('Login') }}
+                    </button>
+                </div>
+
+                <!-- REGISTER IF NO ACCOUNT YET -->
+                <div class="text-center">
+                    <small>Don't have an account yet? <a href="{{ route('register') }}" style="color: chocolate;">{{ __('Register here.') }}</a></small>
+                </div>
+            </form>
         </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+    </div>
+</div>
+@endsection
--- peoplepsyche-app/resources/views/users/client/register.blade.php
+++ peoplepsyche-app/resources/views/users/client/register.blade.php
@@ -1,1 +1,169 @@
-REGISTRATION
+@extends('layout.home')
+
+@section('body')
+<div class="p-5 container d-flex flex-lg-row justify-content-center align-items-center min-vh-100">
+
+    <!-- REGISTER BOX -->
+    <div class="row border rounded-5 p-4 shadow box-area bg-white justify-content-center align-items-center">
+
+        <!-- FORM TITLE -->
+        <div class="row flex-lg-row mb-3">
+            <h1>Registration Form</h1>
+        </div>
+
+        <!-- REGISTER FORM -->
+        <div class="row flex-lg-row justify-content-center align-items-center">
+            <form id="register-form" class="row mb-4 g-3" method="POST" action="{{ route('register') }}">
+                @csrf
+
+                <!-- FIRST NAME -->
+                <div class="col-md-3">
+                    <div class="form-floating overflow-hidden">
+                        <x-text-input type="name" name="givenName" class="form-control" id="floatingGivenName" placeholder="Juan" :value="old('givenName')" required autofocus autocomplete="given-name" />
+                        <x-input-label for="floatingGivenName" :value="__('Given Name')" />
+                        <x-input-error :messages="$errors->get('givenName')" class="mt-2" />
+                    </div>
+                </div>
+
+                <!-- MIDDLE NAME -->
+                <div class="col-md-3">
+                    <div class="form-floating overflow-hidden">
+                        <x-text-input type="name" name="middleName" class="form-control" id="floatingMiddleName" placeholder="Dela" :value="old('middleName')" required autocomplete="additional-name" />
+                        <x-input-label for="floatingMiddleName" :value="__('Middle Name')" />
+                        <x-input-error :messages="$errors->get('middleName')" class="mt-2" />
+                    </div>
+                </div>
+
+                <!-- LAST NAME -->
+                <div class="col-md-3">
+                    <div class="form-floating overflow-hidden">
+                        <x-text-input type="name" name="lastName" class="form-control" id="floatingLastName" placeholder="Cruz" :value="old('lastName')" required autocomplete="family-name" />
+                        <x-input-label for="floatingLastName" :value="__('Last Name')" />
+                        <x-input-error :messages="$errors->get('lastName')" class="mt-2" />
+                    </div>
+                </div>
+
+                <!-- SUFFIX NAME -->
+                <div class="col-md-3">
+                    <div class="form-floating overflow-hidden">
+                        <x-text-input type="name" name="suffixName" class="form-control" id="floatingSuffixName" placeholder="Jr" :value="old('suffixName')" required autocomplete="honorific-suffix" />
+                        <x-input-label for="floatingSuffixName" :value="__('Suffix Name')" />
+                        <x-input-error :messages="$errors->get('suffixName')" class="mt-2" />
+                    </div>
+                </div>
+
+                <!-- EMAIL ADDRESS -->
+                <div class="col-md-6">
+                    <div class="form-floating overflow-hidden">
+                        <x-text-input type="email" name="email" class="form-control" id="floatingEmail" placeholder="name@example.com" :value="old('email')" required autocomplete="email" />
+                        <x-input-label for="floatingEmail" :value="__('Email')" />
+                    </div>
+                </div>
+
+                <!-- PHONE NUMBER -->
+                <div class="col-md-6">
+                    <div class="form-floating overflow-hidden">
+                        <x-text-input type="tel" name="cpNumber" class="form-control" id="floatingCpNumber" placeholder="XXXXXXXXXXX" required autocomplete="tel" />
+                        <x-input-label for="floatingCpNumber" :value="__('Mobile Number')" />
+                    </div>
+                </div>
+
+                <!-- BIRTHDATE -->
+                <div class="col-md-6">
+                    <div class="form-floating overflow-hidden">
+                        <x-text-input type="date" name="birthday" class="form-control" id="floatingBirthday" placeholder="MM/DD/YYYY" required autocomplete="bday" />
+                        <x-input-label for="floatingBirthday" :value="__('Birthday')" />
+                    </div>
+                </div>
+
+                <!-- SEX -->
+                <div class="col-md-6">
+                    <div class="form-floating overflow-hidden">
+                        <select class="form-select" id="floatingSex" name="sex" aria-label="Floating label select example" required>
+                            <option selected disabled>Select Sex</option>
+                            <option value="Male">Male</option>
+                            <option value="Female">Female</option>
+                        </select>
+                        <x-input-label for="floatingSex" :value="__('Sex')" />
+                    </div>
+                </div>
+
+                <!-- CIVIL STATUS -->
+                <div class="col-md-6">
+                    <div class="form-floating overflow-hidden">
+                        <select class="form-select" id="floatingCivilStat" name="civilStat" aria-label="Floating label select example" required>
+                            <option selected disabled>Select Civil Status</option>
+                            <option value="Single">Single</option>
+                            <option value="Married">Married</option>
+                            <option value="Separated">Separated</option>
+                            <option value="Divorced">Divorced</option>
+                            <option value="Widowed">Widowed</option>
+                        </select>
+                        <x-input-label for="floatingCivilStat" :value="__('Civil Status')" />
+                    </div>
+                </div>
+
+                <!-- RELIGION -->
+                <div class="col-md-6">
+                    <div class="form-floating overflow-hidden">
+                        <x-text-input type="text" name="religion" class="form-control" id="floatingReligion" placeholder="Roman Catholic" />
+                        <x-input-label for="floatingReligion" :value="__('Religion')" />
+                    </div>
+                </div>
+
+                <!-- HOME ADDRESS -->
+                <div class="col-md-12">
+                    <div class="form-floating overflow-hidden">
+                        <x-text-input type="text" name="address" class="form-control" id="floatingAddress" placeholder="Home address" required autocomplete="address-line1" />
+                        <x-input-label for="floatingAddress" :value="__('Home Address')" />
+                    </div>
+                </div>
+
+                <!-- USER CODE FOR REGISTRATION -->
+                <div class="col-md-12">
+                    <div class="form-floating overflow-hidden">
+                        <x-text-input type="text" name="user_code" class="form-control" id="floatingUserCode" placeholder="User code" required />
+                        <x-input-label for="floatingUserCode" :value="__('User Code')" />
+                    </div>
+                </div>
+
+                <!-- PASSWORD -->
+                <div class="col-md-12">
+                    <div class="form-floating overflow-hidden">
+                        <x-text-input type="password" name="password" class="form-control" id="floatingPassword" placeholder="Password" required autocomplete="new-password" />
+                        <x-input-label for="floatingPassword" :value="__('Password')" />
+                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                    </div>
+                </div>
+
+                <!-- CONFIRM PASSWORD -->
+                <div class="col-md-12">
+                    <div class="form-floating overflow-hidden">
+                        <x-text-input type="password" name="password_confirmation" class="form-control" id="floatingPasswordConfirm" placeholder="Confirm password" required />
+                        <x-input-label for="floatingPasswordConfirm" :value="__('Confirm Password')" />
+                        <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                    </div>
+                </div>
+
+                <!-- TERMS AND CONDITIONS -->
+                <div class="form-check d-flex justify-content-start ms-2">
+                    <label class="form-check-label text-secondary" for="flexCheckChecked">
+                        <input class="form-check-input" type="checkbox" name="terms_agreement" value="" id="flexCheckChecked" required>
+                        I have read and agree to the <a :href="route('terms')">{{ __('Terms and Condition') }}</a>.
+                    </label>
+                </div>
+
+                <!-- REGISTER BUTTON -->
+                <div class="input-group">
+                    <button class="btn btn-lg w-100 fs-6" type="submit" value="Register">{{ __('Register') }}</button>
+                </div>
+
+                <!-- LOGIN IF HAS ACCOUNT -->
+                <div class="row mb-3 text-center">
+                    <small>Already registered? <a href={{ route('login') }} style="color: chocolate;">{{ __('Login here.') }}</a></small>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+@endsection
