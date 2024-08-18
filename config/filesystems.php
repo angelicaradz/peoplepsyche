22a5ca7755badf0643a96b3b88b6b71d43993120
Angelica Ramirez <angelicaradz3@gmail.com>
2024-08-18 22:29:51 +0800

    went back to original code of filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -32,13 +32,13 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => public_path('/'),
+            'root' => storage_path('app'),
             'throw' => false,
         ],
 
         'public' => [
             'driver' => 'local',
-            'root' => public_path('/'),
+            'root' => storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
