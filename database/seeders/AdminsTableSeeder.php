9ee4e5ca2a3ab3ced0ab9eb21b2176247e9eea67
Angelica Ramirez <angelicaradz3@gmail.com>
2024-08-20 16:17:48 +0800

    modified AdminsTableSeeder.php

--- database/seeders/AdminsTableSeeder.php
+++ database/seeders/AdminsTableSeeder.php
@@ -15,33 +15,33 @@ class AdminsTableSeeder extends Seeder
     public function run(): void
     {
         Admin::insert([
-            [
-                'givenName' => 'Admin',
-                'lastName' => 'Test',
-                'email' => 'angelica.ramirez192001@gmail.com',
-                'cpNumber' => '09123456789',
-                'birthday' => '2024-08-20',
-                'sex' => 'Female',
-                'civilStat' => 'Single',
-                'address' => 'PeoplePsyche',
-                'password' => Hash::make('Admin1234!'),
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'givenName' => 'Cora',
-                'middleName' => 'Estenzo',
-                'lastName' => 'Lim',
-                'email' => 'cora.lim@g.msuiit.edu.ph',
-                'cpNumber' => '09177110980',
-                'birthday' => '1960-06-27',
-                'sex' => 'Female',
-                'civilStat' => 'Married',
-                'address' => 'Marrieta Tower',
-                'password' => Hash::make('Cora1234!'),
-                'created_at' => now(),
-                'updated_at' => now(),
-            ]
+            'givenName' => 'Cora',
+            'middleName' => 'Estenzo',
+            'lastName' => 'Lim',
+            'email' => 'cora.lim@g.msuiit.edu.ph',
+            'cpNumber' => '09177110980',
+            'birthday' => '1960-06-27',
+            'sex' => 'Female',
+            'civilStat' => 'Married',
+            'address' => 'Marrieta Tower',
+            'password' => Hash::make('Cora1234!'),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        Admin::insert([
+            'givenName' => 'Cora',
+            'middleName' => 'Estenzo',
+            'lastName' => 'Lim',
+            'email' => 'cora.lim@g.msuiit.edu.ph',
+            'cpNumber' => '09177110980',
+            'birthday' => '1960-06-27',
+            'sex' => 'Female',
+            'civilStat' => 'Married',
+            'address' => 'Marrieta Tower',
+            'password' => Hash::make('Cora1234!'),
+            'created_at' => now(),
+            'updated_at' => now(),
         ]);
     }
 }
